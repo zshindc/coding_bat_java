de019c7e2b927616c6cf268e8110a2ab5f963c4f
zshindc <88628297+zshindc@users.noreply.github.com>
2021-11-30 14:35:01 -0500

    Update and rename sleepIn to sleepIn.java

--- warmup-1/sleepIn.java
+++ warmup-1/sleepIn.java
@@ -1,3 +1,9 @@
+/* Warmup-1 sleepIn Java Program.
+* 
+* @author Zachary Shin
+* @version 11-3-21
+*/
+
 public boolean sleepIn(boolean weekday, boolean vacation) {
   if (weekday == false || vacation == true) {
     return true;
