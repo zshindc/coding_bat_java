b5d8262e6beae22d7abf47dbb7006abc3ff7ab11
zshindc <88628297+zshindc@users.noreply.github.com>
2021-11-30 15:06:18 -0500

    Create helloName.java

---
+++ string-1/helloName.java
@@ -0,0 +1,9 @@
+/**
+ * CS149 String-1 helloName Program.
+ * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+ * @author Zachary Shin
+ * @version 11-30-2021
+ */
+public String helloName(String name) {
+  return "Hello " + name + "!";
+}
