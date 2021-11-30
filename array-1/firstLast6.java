361235789bd52b6a10c33f17e2ef2359e5936e49
zshindc <88628297+zshindc@users.noreply.github.com>
2021-11-30 15:16:35 -0500

    Create firstLast6.java

---
+++ array-1/firstLast6.java
@@ -0,0 +1,10 @@
+/**
+ * CS149 Coding Bat Array-1 firstLast6 Program.
+ * Given an array of ints, return true if 6 appears as either the first or last element in the array. 
+ * The array will be length 1 or more.
+ * @author Zachary Shin
+ * @version 11-30-21
+ */
+public boolean firstLast6(int[] nums) {
+  return (nums[0] == 6 || nums[nums.length - 1] == 6);
+}
