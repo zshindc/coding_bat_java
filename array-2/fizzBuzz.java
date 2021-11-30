e8e850f21e4b1ac3bf69b784d89c314bb73bffd7
zshindc <88628297+zshindc@users.noreply.github.com>
2021-11-30 15:41:13 -0500

    Create fizzBuzz.java

---
+++ array-2/fizzBuzz.java
@@ -0,0 +1,33 @@
+/**
+ * CS149 Coding Bat String-1 extraEnd Program.
+ * This is slightly more difficult version of the famous FizzBuzz problem which is sometimes given as a first problem for job interviews. (See also: FizzBuzz Code.) 
+ * Consider the series of numbers beginning at start and running up to but not including end, so for example start=1 and end=5 gives the series 1, 2, 3, 4. 
+ * Return a new String[] array containing the string form of these numbers, except for multiples of 3, use "Fizz" instead of the number, for multiples of 5 use "Buzz", and for multiples of both 3 and 5 use "FizzBuzz". 
+ * In Java, String.valueOf(xxx) will make the String form of an int or other type. 
+ * This version is a little more complicated than the usual version since you have to allocate and index into an array instead of just printing, and we vary the start/end instead of just always doing 1..100.
+ * @author Zachary Shin
+ * @version 11-30-21
+ */
+public String[] fizzBuzz(int start, int end) {
+  int n = end - start;
+  String[] result = new String[n];
+  
+  
+  int value = 0;
+  for (int i = start; i < end; i++) {
+    boolean fizz = i % 3 == 0;
+    boolean buzz = i % 5 == 0;
+    
+    if (fizz && buzz) {
+      result[value] = "FizzBuzz";
+    } else if (fizz) {
+      result[value] = "Fizz";
+    } else if (buzz) {
+      result[value] = "Buzz";
+    } else {
+      result[value] = String.valueOf(i);
+    }
+    value++;
+  }
+  return result;
+}
