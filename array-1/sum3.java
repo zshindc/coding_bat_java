a8ec86db0e77674e0a5e6518ef85011acdf4b87f
zshindc <88628297+zshindc@users.noreply.github.com>
2021-11-30 15:20:24 -0500

    Create sum3.java

---
+++ array-1/sum3.java
@@ -0,0 +1,9 @@
+/**
+ * CS149 Coding Bat array-1 sum3 Program.
+ * Given an array of ints length 3, return the sum of all the elements.
+ * @author Zachary Shin
+ * @version 11-30-21
+ */
+public int sum3(int[] nums) {
+  return nums[0] + nums[1] + nums[2];
+}
