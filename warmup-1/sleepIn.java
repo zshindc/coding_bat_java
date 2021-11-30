c5309efd166e15ed112e9e8023d354556cca588a
zshindc <88628297+zshindc@users.noreply.github.com>
2021-11-30 14:43:06 -0500

    Update sleepIn.java

--- warmup-1/sleepIn.java
+++ warmup-1/sleepIn.java
@@ -4,6 +4,11 @@
 * @version 11-3-21
 */
 
+/* sleepIn class. 
+* @param weekday is when it is a weekday (true) or not (false)
+* @param vacation is when we are on vacation
+* @return sleep in if not a weekday or on vacation (true)
+*/
 public boolean sleepIn(boolean weekday, boolean vacation) {
   if (weekday == false || vacation == true) {
     return true;
